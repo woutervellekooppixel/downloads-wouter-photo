fb5f9e0e0823d4abcc80ace4957cc940409e7287
woutervellekooppixel <info@woutervellekoop.nl>
2025-07-07 21:19:51 +0200

    Update page.tsx

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { notFound } from 'next/navigation';
-import HeroSection from '../../components/HeroSection';
+import HeroSection from '../components/HeroSection';
 import Image from 'next/image';
 
 export default function Page({ params }: { params: { slug: string } }) {
