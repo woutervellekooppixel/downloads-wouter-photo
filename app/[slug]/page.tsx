e3b0ec6069cbd688c938920d887af972c416f634
woutervellekooppixel <info@woutervellekoop.nl>
2025-07-07 22:03:53 +0200

    Update page.tsx

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -60,3 +60,18 @@ export default function Page({ params }: Props) {
     </div>
   );
 }
+
+export async function generateStaticParams() {
+  const photosDir = path.join(process.cwd(), "public", "photos");
+
+  if (!fs.existsSync(photosDir)) return [];
+
+  const folders = fs
+    .readdirSync(photosDir, { withFileTypes: true })
+    .filter((entry) => entry.isDirectory())
+    .map((entry) => ({
+      slug: entry.name,
+    }));
+
+  return folders;
+}
