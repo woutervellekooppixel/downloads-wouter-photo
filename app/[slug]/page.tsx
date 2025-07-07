b9f8c0296e5499c05318ba45f3b3072896571762
woutervellekooppixel <info@woutervellekoop.nl>
2025-07-07 21:04:55 +0200

    Update page.tsx

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -2,9 +2,16 @@ import fs from "fs";
 import path from "path";
 import { notFound } from "next/navigation";
 import HeroSection from "../components/HeroSection";
+import Image from "next/image";
 
-export default function Page(props: any) {
-  const slug = props.params.slug;
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function Page({ params }: PageProps) {
+  const slug = params.slug;
   const folderPath = path.join(process.cwd(), "public", "photos", slug);
 
   if (!fs.existsSync(folderPath)) {
@@ -28,9 +35,11 @@ export default function Page(props: any) {
         <div className="max-w-4xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
           {files.map((file) => (
             <div key={file} className="relative group overflow-hidden rounded shadow">
-              <img
+              <Image
                 src={`/photos/${slug}/${file}`}
                 alt={file}
+                width={600}
+                height={400}
                 className="w-full h-auto transition-transform duration-300 group-hover:scale-105"
               />
               <div className="absolute inset-0 bg-black/40 opacity-80 flex items-center justify-center sm:opacity-0 sm:group-hover:opacity-80 transition duration-300">
