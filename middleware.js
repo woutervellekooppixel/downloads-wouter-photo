e9036755e657da1142f44644952ffcd0fef7fa5d
woutervellekooppixel <info@woutervellekoop.nl>
2025-07-09 23:53:37 +0200

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const basicAuth = request.headers.get('authorization')
+
+  const expectedAuth = 'Basic ' + Buffer.from(
+    `${process.env.ADMIN_USER}:${process.env.ADMIN_PASS}`
+  ).toString('base64')
+
+  if (basicAuth === expectedAuth) {
+    return NextResponse.next()
+  }
+
+  return new NextResponse('Unauthorized', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Beheer"',
+    },
+  })
+}
+
+export const config = {
+  matcher: ['/beheer'],
+}
